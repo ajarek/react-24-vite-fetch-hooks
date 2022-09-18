f26e5c08236da3e9ef1d4fb5f8a84200fa859f0c
ajarek <ajarek@poczta.onet.pl>
2022-09-19 00:59:02 +0200

    added Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,54 @@
-import {React ,useEffect, useState} from 'react'
+import { React, useEffect, useState } from 'react'
 import "./App.css"
 import { Navbar } from './components/Navbar/Navbar'
 import { ListBlog } from './components/ListBlog/ListBlog'
 import { WrapperLayout } from './components/WrapperLayout/WrapperLayout'
 import { FetchData } from './components/FetchData/FetchData'
-// import data from './assets/data/data.json'
+import { FullPageLayout } from './components/FullPageLayout/FullPageLayout'
+import { Loading } from './components/Loading/Loading'
+
 
 function App() {
 
-  
+
   const [blogs, setBlogs] = useState(null);
   const [pending, setPending] = useState(true);
- 
-useEffect(() => {
-  // npx json-server --watch src/assets/data/data.json --port 8000
-const url='http://localhost:8000/blogs'
-FetchData(url)
-  .then((data) => setBlogs(data))
-  .catch((err) => {
-    console.error('Error:', err)
-  })
-  .finally(()=>setPending(false))
-  
-}, []);
+
+  useEffect(() => {
+    // npx json-server --watch src/assets/data/data.json --port 8000
+    const url = 'http://localhost:8000/blogs'
+    setTimeout(() => {
+      FetchData(url)
+      .then((data) => setBlogs(data))
+      .catch((err) => {
+        console.error('Error:', err)
+      })
+      .finally(() => setPending(false))
+    }, "2000")
+  }, []);
 
   return (
     <div className="App">
-     { pending?<div>Loading...</div>:null}
+
+      {pending ?
+        <FullPageLayout>
+          <Loading />
+        </FullPageLayout>
+        : null}
+
       <Navbar />
       <WrapperLayout
-      style={{
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        overflow: 'auto',
-        height:'90vh',
-        width:'100%' ,
-      }}
+        style={{
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          overflow: 'auto',
+          height: '90vh',
+          width: '100%',
+        }}
       >
-      <ListBlog
-      blogs={blogs}
-      />
+        <ListBlog
+          blogs={blogs}
+        />
       </WrapperLayout>
     </div>
   )
