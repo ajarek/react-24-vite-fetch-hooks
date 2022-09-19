d71eb3c0b0df4faccb1d1df5fe5c1a12f867a524
ajarek <ajarek@poczta.onet.pl>
2022-09-20 00:15:23 +0200

    format document

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,41 +1,34 @@
-import { React, useEffect, useState } from 'react'
-import { ListBlog } from '../../components/ListBlog/ListBlog'
-import { WrapperLayout } from '../../components/WrapperLayout/WrapperLayout'
-import { useFetch } from '../../components/useFetch/useFetch'
-import { FullPageLayout } from '../../components/FullPageLayout/FullPageLayout'
-import { Loading } from '../../components/Loading/Loading'
-import classes from './styles.module.css'
+import { React } from "react"
+import { ListBlog } from "../../components/ListBlog/ListBlog"
+import { useFetch } from "../../components/useFetch/useFetch"
+import { FullPageLayout } from "../../components/FullPageLayout/FullPageLayout"
+import { Loading } from "../../components/Loading/Loading"
+import classes from "./styles.module.css"
 
 export const Home = (props) => {
+  const { className, ...otherProps } = props
+
+  // npx json-server --watch src/assets/data/data.json --port 8000
+
   const {
-    className,
-    ...otherProps
-  } = props
-  
-// npx json-server --watch src/assets/data/data.json --port 8000
-
- 
-const{data:blogs, pending, error}=useFetch("http://localhost:8000/blogs")
-  
-
- 
+    data: blogs,
+    pending,
+    error,
+  } = useFetch("http://localhost:8000/blogs")
 
   return (
     <div
-      className={`${classes.root}${className ? ` ${className}` : ''}`}
+      className={`${classes.root}${className ? ` ${className}` : ""}`}
       {...otherProps}
     >
-{error? <div>{error}</div>:null}
-      {pending ?
+      {error ? <div>{error}</div> : null}
+      {pending ? (
         <FullPageLayout>
           <Loading />
         </FullPageLayout>
-        : null}
-       { blogs &&<ListBlog  blogs={blogs}
-      />}
-
+      ) : null}
+      {blogs && <ListBlog blogs={blogs} />}
     </div>
   )
-
 }
 export default Home
